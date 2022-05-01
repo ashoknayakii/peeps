9601abf03bb26164f11bc479a5b9f4d2914fa7ed
Ashok Nayak <ashoknayak@MacBook-Air.local>
2022-05-01 17:08:13 -0400

    server running successfully with addition of index js in models

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
 router.use('/thoughts', thoughtRoutes);
-// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+
 router.use('/users', userRoutes);
 
 module.exports = router;
